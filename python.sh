f599a17548aaf17dc6dae36d4f75ed9c5ba4f37f
Matthieu Court <matthieu.court@yoppworks.com>
2022-04-22 14:23:11 -0400

    Remove wrong installation of pip.

--- python.sh
+++ python.sh
@@ -12,7 +12,6 @@ case $PLATFORM in
     brew install pyenv
     pyenv install $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
     pyenv global $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
-    brew install pip
     brew install jupyter
     ;;
 
